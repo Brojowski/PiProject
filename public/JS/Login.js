b8c2cd04aa5e71a6347b9ed97399f0f330a6494a
rossc <cwross204>
2016-01-11 20:55:29 -0500

    Added Angular to the login page

--- public/JS/Login.js
+++ public/JS/Login.js
@@ -5,4 +5,31 @@
 (function(){
     var app = angular.module('LoginScreen',[]);
 
+    app.controller('LoginController', function(){
+
+        this.loginScreen = true;
+
+        this.username = "asdf";
+        this.password = "test";
+        this.twoFactorID = "";
+
+        this.loginButtonClick = function(){
+            console.log(this.username + " " + this.password);
+
+            //TODO: Check to make sure the username and password is valid
+            //For now assume its true
+
+            this.loginScreen = false;
+        }
+
+        this.twoFactorIDButtonClick = function(){
+            console.log(this.twoFactorID);
+
+            //TODO: Check to make sure 2FA is correct
+            //For now assume its true
+        }
+    });
+
+
+
 })();
